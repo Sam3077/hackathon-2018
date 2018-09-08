60a80d0a433c89e56cee79ee343537b882dd003a
Sam Hirsch <sam.hirsch66@gmail.com>
2018-09-08 17:17:38 -0600

    added color palette

---
+++ src/Colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+  // UI Colors
+  DarkBackground: "#2C302E",
+  Gray: "#474A48",
+  LightGray: "#909590",
+  LightGreen: "#9AE19D",
+  DarkGreen: "#537A5A",
+
+  // Text Colors
+  StandardText: "#f9f9f9",
+  DarkText: "#141615"
+};
+
+export default Colors;
