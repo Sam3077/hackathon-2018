c5846c1eb17c30ff9dd832237da895b681a06175
alexCharters <alexcharters45@gmail.com>
2018-09-08 21:46:17 -0600

    added navbar to GroupsList

---
+++ src/pages/GroupsList.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import styled from "styled-components";
+import Colors from "../Colors";
+import Input from "@material-ui/core/Input"
+
+const Background = styled.div`
+  background-color: ${Colors.DarkBackground};
+  height: 100vh;
+  width: 100vw;
+`;
+
+const NavBar = styled.nav`
+    width: 100%;
+    height: 75px;
+    background-color: ${Colors.LightGray};
+`
+const NavBarLeft = styled.nav`
+    float: left;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+`
+
+const NavBarRight = styled.nav`
+    font-size: 5em;
+    padding: 20px;
+    float: right;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 88%;
+`
+
+const ProfPic = styled.img`
+    margin: 10px;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    border: ${Colors.DarkBackground} 2px solid;
+`
+const Search = styled(Input)`
+    height: 30px;
+    width: 100%;
+`
+
+class GroupsList extends React.Component {
+    public state = {
+
+    };
+
+    public render() {
+        return (
+            <Background>
+                <NavBar>
+                    <NavBarLeft>
+                        <ProfPic src={require('../pictures/default.png')} />
+                    </NavBarLeft>
+                    <NavBarRight>
+                        <Search type="search"/>
+                    </NavBarRight>
+                </NavBar>
+            </Background>
+        );
+    }
+}
+
+export default GroupsList;
