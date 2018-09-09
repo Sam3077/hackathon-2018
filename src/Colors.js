a8a66384392d007ef17f56e9ab293f53e9f2f3e4
Sam Hirsch <sam.hirsch66@gmail.com>
2018-09-08 19:00:37 -0600

    starting firebase auth ui

--- src/Colors.js
+++ src/Colors.js
@@ -1,14 +1,14 @@
 const Colors = {
   // UI Colors
   DarkBackground: "#2C302E",
+  DarkGreen: "#537A5A",
+  DarkText: "#141615",
   Gray: "#474A48",
   LightGray: "#909590",
   LightGreen: "#9AE19D",
-  DarkGreen: "#537A5A",
 
   // Text Colors
-  StandardText: "#f9f9f9",
-  DarkText: "#141615"
+  StandardText: "#f9f9f9"
 };
 
 export default Colors;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import './index.css';
-import Starter from './pages/Starter';
+import SignIn from './pages/SignIn';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 	<Router>
 		<div>
-			<Route exact={true} path="/" component={Starter} />
+			<Route exact={true} path="/" component={SignIn} />
 		</div>
 	</Router>,
 	document.getElementById('root') as HTMLElement
