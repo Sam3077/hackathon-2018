bd095b2eb35a197a7d3dfb513546fca75e3fe2d1
alexCharters <alexcharters45@gmail.com>
2018-09-09 12:00:34 -0600

    Scanned Reciepts now show up in the fields of the new transaction view

--- src/pages/NewTransaction.tsx
+++ src/pages/NewTransaction.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import {List, Button, IconButton} from '@material-ui/core';
+import { List, Button, IconButton } from '@material-ui/core';
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
 import TransactionListItem from '../components/TransactionListItem';
 import styled from 'styled-components';
 import Colors from "../Colors";
+//import { array, object } from 'prop-types';
 
 const Background = styled.div`
   background-color: ${Colors.DarkBackground};
@@ -12,16 +13,64 @@ const Background = styled.div`
   width: 100vw;
 `;
 
+function isNormalInteger(str: string) {
+    var n = Math.floor(Number(str));
+    return n !== Infinity && String(n) === str && n >= 0;
+}
+
 class NewTransaction extends React.Component {
-    itemsArray = [{name: '', amount: ''}];
-    props: {location: any}
+    itemsArray = [{ name: '', amount: '' }];
+    props: { location: any }
 
     componentDidMount() {
         const data = this.props.location.state.incomingData;
-        for (let i = 0; i < data.responses.length; i++) {
-            console.log(data.responses[i]);
+        const text = data.responses[0].textAnnotations;
+        //let letters: any = [];
+        let item = "";
+        let price = "";
+        let letters = [];
+        for (let i = 1; i < text.length; i++) {
+            var word = text[i].description;
+            for (let j = 0; j < word.length; j++) {
+                let letter = text[i].description[j];
+                letters.push(letter);
+            }
+            letters.push(" ");
+        }
+
+
+
+
+        console.log(letters)
+        for (var i = 0; i < letters.length; i++) {
+            var letter = letters[i];
+            console.log(letter == '$')
+            if (letter == '$' || letter == 'S') {
+                let count = 1;
+                while (isNormalInteger(letters[i+count])) {
+                    price += letters[i+count];
+                    count++;
+                }
+                if (letters[i+count] == "," || letters[i+count] == ".") {
+                    price += letters[i+count + 1] + letters[i+count + 2];
+                    console.log(item + ": " + price);
+                    this.itemsArray.push({ name: item, amount: (parseInt(price)/100).toFixed(2)});
+                    this.forceUpdate();
+                    price = "";
+                    item = "";
+                    i = i + count+2;
+                }
+                else {
+                    count = 1;
+                    price = "";
+                }
+            }
+            else {
+                item += letter;
+            }
         }
     }
+
     render() {
         return (
             <Background>
@@ -31,15 +80,15 @@ class NewTransaction extends React.Component {
                             console.log(key);
                             this.itemsArray = this.itemsArray.slice(0, key).concat(this.itemsArray.slice(key + 1, this.itemsArray.length));
                             this.forceUpdate();
-                        }}/>
+                        }} />
                     ))}
-                    <IconButton style={{marginLeft: "20px"}} onClick={() => {
-                        this.itemsArray.push({name: '', amount: ''});
+                    <IconButton style={{ marginLeft: "20px" }} onClick={() => {
+                        this.itemsArray.push({ name: '', amount: '' });
                         this.forceUpdate();
-                    }}><AddCircleOutline style={{color: Colors.StandardText}}/></IconButton>
+                    }}><AddCircleOutline style={{ color: Colors.StandardText }} /></IconButton>
                 </List>
-                <Button 
-                    style={{backgroundColor: Colors.LightGreen, borderRadius: "5px", marginLeft: "5px"}}><h3 style={{margin: "0px"}}>Save</h3></Button>
+                <Button
+                    style={{ backgroundColor: Colors.LightGreen, borderRadius: "5px", marginLeft: "5px" }}><h3 style={{ margin: "0px" }}>Save</h3></Button>
             </Background>
         )
     }
